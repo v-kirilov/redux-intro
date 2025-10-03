96ca11028b34885910bd4f47e5cf15cc4aec49b4
v-kirilov <v_kirilov87@protonmail.ch>
2025-10-03 09:19:02 +0000

    Connect Redux to our app.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import store from "./store"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import store from "./store";
 
-store.dispatch({ type: 'account/deposit', payload: 1000 });
-console.log(store.getState());
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
